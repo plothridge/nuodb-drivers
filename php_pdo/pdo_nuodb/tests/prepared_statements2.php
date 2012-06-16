971d42365d8451ddf605f2bee64a773e7c27db18
Thomas Gates <tom@thebithead.com>
2012-06-16 09:35:15 -0400

    Adding tests

---
+++ php_pdo/pdo_nuodb/tests/prepared_statements2.php
@@ -0,0 +1,20 @@
+<?php 
+try {  
+  $db = new PDO("nuodb:database=test@localhost;schema=Hockey", "dba", "goalie") or die;
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $position = 'Defense';
+  $sql = "select count(*) from hockey where POSITION = :position";
+  $stmt = $db->prepare($sql);
+  $stmt->bindParam(':position', $position, PDO::PARAM_STR);
+  $stmt->execute();
+  $result = $stmt->fetchAll();
+  foreach ($result as $row) {
+     print_r ($row);
+  }
+  $db = NULL;
+} catch(PDOException $e) {  
+  echo $e->getMessage();  
+}
+$db = NULL;  
+echo "done\n";
+?>
