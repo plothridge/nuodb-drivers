b2ac4a28bca63d26503a8e136b1aad38d1e730ce
Thomas Gates <tom@thebithead.com>
2012-07-18 10:45:26 -0400

    Port to Windows

--- php_pdo/do_win32.bat
+++ php_pdo/do_win32.bat
@@ -11,8 +11,8 @@ rmdir /Q /S Debug
 rmdir /Q /S RelWithDebInfo
 rmdir /Q /S php_nuodb.dir
 del /Q cmake_install.cmake
-del /Q NUOPHP.suo
-cmake -G "Visual Studio 9 2008" -D NUODB_ROOT=%NUODB_ROOT% -D NUODB_INCLUDE_DIR=%NUODB_ROOT%/include -D NUODB_LIB_DIR=%NUODB_ROOT%/lib -D NUODB_REMOTE_LIBRARY=%NUODB_ROOT%/lib -D PHP_ROOT=%PHP_INSTALL_DIR% -D PHP_INCLUDE_DIR=%PHP_INSTALL_DIR%/SDK/include .
+del /Q NuoPhpPdo.suo
+cmake -G "Visual Studio 10" -D NUODB_ROOT=%NUODB_ROOT% -D NUODB_INCLUDE_DIR=%NUODB_ROOT%/include -D NUODB_LIB_DIR=%NUODB_ROOT%/lib -D NUODB_REMOTE_LIBRARY=%NUODB_ROOT%/lib -D PHP_ROOT=%PHP_INSTALL_DIR% -D PHP_INCLUDE_DIR=%PHP_INSTALL_DIR%/SDK/include .
 devenv NuoPhpPdo.sln /build Debug /project NuoPhpPdo
 devenv NuoPhpPdo.sln /build RelWithDebInfo /project NuoPhpPdo
 copy /Y Debug\*.* %PHP_INSTALL_DIR%\debug
